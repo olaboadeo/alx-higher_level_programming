c1d11fea220a3e27b98db331da3211f2e38c43a7
Esther Olaboade <olaboadeo@gmail.com>
2023-11-07 01:52:40 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return len(sentence), None
     else:
-        return len(sentence), sentence[]
+        return len(sentence), sentence[0]
