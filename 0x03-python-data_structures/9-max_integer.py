9ed281cfed086707dfd036bfe392737822a0c4af
Esther Olaboade <olaboadeo@gmail.com>
2023-11-07 01:57:34 -0800

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    x = my_list[0]
+    for i in my_list:
+        if i > x:
+            x = i
+        else:
+            continue
+    return x
