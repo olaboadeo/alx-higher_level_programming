085546f01a870fdfa098ab0a22ab413a7456370f
Esther Olaboade <olaboadeo@gmail.com>
2023-11-21 18:33:20 -0800

    task 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    total = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            total += 1
-        except (ValueError, TypeError):
-            pass
-    print()
-    return total
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
