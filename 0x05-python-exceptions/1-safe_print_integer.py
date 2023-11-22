38e6da02d2a508bc2f32ba7141145da7030f4488
Esther Olaboade <olaboadeo@gmail.com>
2023-11-21 18:18:55 -0800

    task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    total = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            total += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return total
