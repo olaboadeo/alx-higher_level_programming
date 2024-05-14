948efde862913ed9ee96b7c5d0dc9a0eb6fddccf
Esther Olaboade <olaboadeo@gmail.com>
2024-05-14 14:03:09 +0100

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,7 @@ class Square:
         self.__size = size
 
     @property
-    def size(sef):
+    def size(self):
         """Get/set the current size of the square"""
         return (self.__size)
 
